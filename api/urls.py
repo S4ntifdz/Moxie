5edc2411460c2d308e5b7e309dc72782a3fb6b79
S4ntifdz <safernandez@frro.utn.edu.ar>
2025-02-24 19:28:04 -0300

    auth

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,10 @@ from api.views.services.service_category_view import ServiceCategoryView
 from api.views.services.service_type_view import ServiceTypeView
 from api.views.services.service_view import ServiceView
 from api.views.suppliers.supplier_view import SupplierView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'medspas', MedspaView)
@@ -20,4 +23,6 @@ router.register(r'suppliers', SupplierView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views/services/service_category_view.py
+++ api/views/services/service_category_view.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
 from api.serializers.services.service_category_serializer import ServiceCategorySerializer
-from api.serializers.services.service_type_serializer import ServiceTypeSerializer
 from service_management.models.service_category_model import ServiceCategoryModel
+from rest_framework.permissions import IsAuthenticated
 
 
 class ServiceCategoryView(viewsets.ModelViewSet):
     queryset = ServiceCategoryModel.objects.all()
     serializer_class = ServiceCategorySerializer
+    permission_classes = [IsAuthenticated]
