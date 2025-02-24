2d6adc5a0482164d4a0806eb0cf05baee22b46d1
S4ntifdz <safernandez@frro.utn.edu.ar>
2025-02-24 18:01:36 -0300

    Pagination: products

---
+++ api/views/products/product_view.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+from api.serializers.products.product_serializer import ProductSerializer
+from products.models import ProductModel
+from rest_framework.pagination import PageNumberPagination
+
+class ProductPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class ProductView(viewsets.ModelViewSet):
+    queryset = ProductModel.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = ProductPagination
--- app/settings.py
+++ app/settings.py
@@ -131,3 +131,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
